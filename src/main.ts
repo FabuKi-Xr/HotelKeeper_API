8bcf17597d185939673180bf4d2ad4cb29474f55
Charonyx <churairat.onn@gmail.com>
2022-11-22 21:31:36 +0700

    update ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
     app.enableCors();
-    await app.listen(4002);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+        }),
+    );
+
+    await app.listen(3000);
 }
 bootstrap();
